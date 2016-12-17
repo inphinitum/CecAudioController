a399b9eb540f561879d7022f89fb5bf0d71810ae
Javi Martínez <javi@flamingalah.net>
2016-12-17 17:57:10 +0100

    Increased test coverage

--- test/cec_audio_controller_test.py
+++ test/cec_audio_controller_test.py
@@ -15,92 +15,115 @@ limitations under the License.
 """
 
 import unittest
-import subprocess
-from threading import Timer
 from unittest.mock import Mock
-
-from cec_audio_controller.device_controller import DeviceController
 
 
 class DeviceHandlerTest(unittest.TestCase):
 
-    @unittest.mock.patch("subprocess.Popen", autospec=True)
-    def test_power_on(self, mock_popen):
+    def test_power_on(self):
         """
-            Test single command power_on.
+        Test single command power_on.
 
-        :param mock_popen: the mock object for Popen
         :return: nothing
         """
 
-        controller = self.init_controller(mock_popen)
+        controller = self.init_controller()
 
         # Control that the command to power on the audio device is sent, and timer not cancelled
-        mock_popen.communicate.return_value = ("", "")
+        controller._cec_process.communicate.return_value = ("", "")
         controller.power_on()
-        mock_popen.communicate.assert_called_with(input="on 5", timeout=15)
+        controller._cec_process.communicate.assert_called_with(input="on 5", timeout=15)
 
-    @unittest.mock.patch("subprocess.Popen", autospec=True)
-    def test_standby(self, mock_popen):
+    def test_standby(self):
         """
-            Test single command standby.
+        Test single command standby.
 
-        :param mock_popen: the mock object for Popen
         :return: nothing
         """
 
-        controller = self.init_controller(mock_popen)
+        controller = self.init_controller()
 
         # Control that the command to standby the audio device is sent, and timer not cancelled
-        mock_popen.communicate.return_value = ("", "")
+        controller._cec_process.communicate.return_value = ("", "")
         controller.standby()
-        mock_popen.communicate.assert_called_with(input="standby 5", timeout=15)
+        controller._cec_process.communicate.assert_called_with(input="standby 5", timeout=15)
 
-    @unittest.mock.patch("subprocess.Popen", autospec=True)
-    def test_power_on_with_delayed_stby(self, mock_popen):
+    def test_power_on_with_delayed_stby(self):
         """
-            Test single command power_on while there was a pending delayed_standby.
+        Test single command power_on while there was a pending delayed_standby.
 
-        :param mock_popen: the mock object for Popen
-        :param mock_timer: the mock object for Timer
         :return: nothing
         """
 
-        controller = self.init_controller(mock_popen)
+        controller = self.init_controller()
         mock_timer = Mock()
         controller._standby_timer = mock_timer
 
         # Control that the command to power on the audio device is sent, and timer cancelled
-        mock_popen.communicate.return_value = ("", "")
+        controller._cec_process.communicate.return_value = ("", "")
         controller.power_on()
-        mock_popen.communicate.assert_called_with(input="on 5", timeout=15)
+        controller._cec_process.communicate.assert_called_with(input="on 5", timeout=15)
         mock_timer.cancel.assert_called_with()
         self.assertIsNone(controller._standby_timer)
 
-    @unittest.mock.patch("subprocess.Popen", autospec=True)
-    def test_standby_with_delayed_stby(self, mock_popen):
+    def test_standby_with_delayed_stby(self):
         """
-            Test single command power_on while there was a pending delayed_standby.
+        Test single command power_on while there was a pending delayed_standby.
 
-        :param mock_popen: the mock object for Popen
-        :param mock_timer: the mock object for Timer
         :return: nothing
         """
 
-        controller = self.init_controller(mock_popen)
+        controller = self.init_controller()
         mock_timer = Mock()
         controller._standby_timer = mock_timer
 
         # Control that the command to power on the audio device is sent, and timer cancelled
-        mock_popen.communicate.return_value = ("", "")
+        controller._cec_process.communicate.return_value = ("", "")
         controller.standby()
-        mock_popen.communicate.assert_called_with(input="standby 5", timeout=15)
-        mock_timer.cancel.assert_called_with()
+        controller._cec_process.communicate.assert_called_with(input="standby 5", timeout=15)
+        mock_timer.cancel.assert_called_once_with()
         self.assertIsNone(controller._standby_timer)
 
+    @unittest.mock.patch("threading.Timer", autospec=True)
+    def test_delayed_standby(self, mock_timer):
+        """
+        Test single command delayed_standby.
+
+        :param mock_timer: the mock object for Timer
+        :return: nothing
+        """
+
+        controller = self.init_controller()
+        mock_timer.return_value = mock_timer
+
+        controller.delayed_standby(10)
+        mock_timer.start.assert_called_once_with()
+        mock_timer.reset_mock()
+
+    @unittest.mock.patch("threading.Timer", autospec=True)
+    def test_delayed_standby_with_delayed_standby(self, mock_timer):
+        """
+        Test single command delayed_standby when there was a pending delayed_standby.
+
+        :param mock_timer: the mock object for Timer
+        :return: nothing
+        """
+
+        mock_timer.reset_mock()
+        controller = self.init_controller()
+        mock_timer.return_value = mock_timer
+
+        # Setup previous timer...
+        controller._standby_timer = mock_timer
+
+        controller.delayed_standby(10)
+        mock_timer.cancel.assert_called_once_with()
+        controller._standby_timer.start.assert_called_once_with()
+
+    @unittest.mock.patch("subprocess.Popen", autospec=True)
     def init_controller(self, mock_popen):
         """
-            Boilerplate code to initialize the controller.
+        Boilerplate code to initialize the controller.
 
         :param mock_popen: the mock object for Popen
         :return: the newly created controller
@@ -109,8 +132,10 @@ class DeviceHandlerTest(unittest.TestCase):
         mock_popen.communicate.return_value = ("logical address 5", "")
 
         # Control the cec-client is invoked properly, audio device searched and found
+        import subprocess
+        from cec_audio_controller.device_controller import DeviceController
         controller = DeviceController()
-        mock_popen.assert_called_with(["cec-client"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        mock_popen.communicate.assert_called_with(input="lad", timeout=15)
+        mock_popen.assert_called_once_with(["cec-client"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        mock_popen.communicate.assert_called_once_with(input="lad", timeout=15)
 
         return controller
